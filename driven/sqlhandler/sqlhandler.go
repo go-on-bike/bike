2eeb4ab3365c104250dbbb34d2ec926d4656e3cb
matisin <matias.medina.s2@gmail.com>
2025-01-15 16:57:53 -0300

    callig both set db when function is called

--- driven/sqlhandler/sqlhandler.go
+++ driven/sqlhandler/sqlhandler.go
@@ -1,5 +1,7 @@
 package sqlhandler
 
+import "database/sql"
+
 type SQLHandler struct {
 	*Connector
 	*Migrator
@@ -8,6 +10,7 @@ type SQLHandler struct {
 func NewDataHandler(connOpts []ConnOption, migrOpts []MigrOption) *SQLHandler {
 	c := NewConnector(connOpts...)
 	m := NewMigrator(nil, migrOpts...)
+
 	return &SQLHandler{Connector: c, Migrator: m}
 }
 
@@ -25,4 +28,13 @@ func (h *SQLHandler) Close() error {
 	}
 	h.Migrator.db = nil
 	return nil
+}
+
+func (h *SQLHandler) SetDB(db *sql.DB) {
+    h.Connector.SetDB(db)
+    h.Migrator.SetDB(db)
+}
+
+func (h *SQLHandler) DB() *sql.DB {
+    return h.Connector.DB()
 }
