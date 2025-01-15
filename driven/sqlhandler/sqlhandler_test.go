646572791422c5745ceb371e6a9e216ab501afc1
matisin <matias.medina.s2@gmail.com>
2025-01-15 11:38:56 -0300

    added test of happy path for connecting and migrating a libsql db

--- driven/sqlhandler/sqlhandler_test.go
+++ driven/sqlhandler/sqlhandler_test.go
@@ -3,6 +3,7 @@ package sqlhandler
 import (
 	"testing"
 
+	"github.com/go-on-bike/bike/interfaces"
 	_ "github.com/tursodatabase/go-libsql"
 )
 
@@ -10,33 +11,37 @@ func TestSQLHandlerComposition(t *testing.T) {
 	dbURL, dbPath := GetDBLocation(t)
 	migrPATH := GetMigrationPATH(t)
 
-	handler := NewHandler([]ConnOption{WithURL(dbURL)}, []MigrOption{WithPATH(migrPATH)})
+	var handler interfaces.DataHandler = NewDataHandler(
+		[]ConnOption{WithURL(dbURL)},
+		[]MigrOption{WithPATH(migrPATH)},
+	)
 
 	if err := handler.Connect("libsql"); err != nil {
 		t.Fatalf("failed to connect to database: %v", err)
 	}
-	t.Log(handler.Connector.DB)
-	t.Log(handler.Migrator.DB)
-	// t.Log(*handler.db)
 
-	// t.Log(handler.DB)
-	// t.Log(&handler.DB)
-
-	if err := handler.Move(false, 0); err != nil {
+	if err := handler.Move(0, false); err != nil {
 		t.Fatalf("error on 1st migration: %v", err)
 	}
 
-	// if err := handler.Move(true, 0); err != nil {
-	// t.Fatalf("error on 2nd migration: %v", err)
-	// }
+	if err := handler.Move(0, true); err != nil {
+		t.Fatalf("error on 2nd migration: %v", err)
+	}
 
-	// if err := handler.Move(false, 0); err != nil {
-	// t.Fatalf("error on 3rd migration: %v", err)
-	// }
+	if err := handler.Move(0, false); err != nil {
+		t.Fatalf("error on 3rd migration: %v", err)
+	}
 
-	// if err := handler.Close(); err != nil {
-	// t.Fatalf("error closing database: %v", err)
-	// }
+	version, err := handler.Version()
+	if err != nil {
+		t.Fatalf("error getting db version: %v", err)
+	}
+
+	t.Logf("Version of db is %d", version)
+
+	if err := handler.Close(); err != nil {
+		t.Fatalf("error closing database: %v", err)
+	}
 
 	AssertDBState(t, dbPath)
 }
