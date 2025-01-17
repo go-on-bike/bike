d76163bf830d21bf86eedbb7123373541359b39a
matisin <matias.medina.s2@gmail.com>
2025-01-17 11:33:11 -0300

    ordered regexp for readability

--- validator/validator.go
+++ validator/validator.go
@@ -7,12 +7,14 @@ import (
 	"unicode/utf8"
 )
 
+type ValidationFunc func(interface{}) bool
+
 // Expresiones regulares para validaciones comunes.
 var (
+    digitRX = regexp.MustCompile(`^[0-9]+$`)
+    rutRX   = regexp.MustCompile(`^[0-9]{1,8}-[0-9Kk]$`)
+    uuidRX  = regexp.MustCompile("^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$")
 	emailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-	uuidRX  = regexp.MustCompile("^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$")
-	rutRX   = regexp.MustCompile(`^[0-9]{1,8}-[0-9Kk]$`)
-	digitRX = regexp.MustCompile(`^[0-9]+$`)
 )
 
 func NotEmpty(value string) bool {
